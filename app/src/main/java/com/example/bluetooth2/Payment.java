ef3a76e55f58559f679a80e51bbb71d23011249d
madinaa1467 <madinaa1467@gmail.com>
2020-07-29 23:47:28 +0600

    solved build problems show proper text in textView

--- app/src/main/java/com/example/bluetooth2/Payment.java
+++ app/src/main/java/com/example/bluetooth2/Payment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Payment extends AppCompatActivity {
 
@@ -15,14 +16,14 @@ public class Payment extends AppCompatActivity {
         setContentView(R.layout.activity_payment);
 
         pressedButtonNumber = getIntent().getExtras().getInt("buttonNumber");
-//        TextView tv1 = (TextView)findViewById(R.id.textView);
+        TextView tv1 = (TextView)findViewById(R.id.textView2);
 
 //        TextView tv1 = (TextView) this.findViewById(R.id.textView1);
 
         switch (pressedButtonNumber) {
             case 1:
 //                tv1.setText("new text new text");
-//                tv1.setText("" + getString(R.string.btn_shampoo));
+                tv1.setText("" + getString(R.string.btn_shampoo));
                 System.err.println("!!! Button 1" + getString(R.string.btn_shampoo));
                 break;
             case 2:
