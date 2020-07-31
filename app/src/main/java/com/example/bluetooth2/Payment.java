35cd883d1aa6b0ffa2f9a9f6590127bc2b914857
madinaa1467 <madinaa1467@gmail.com>
2020-07-31 22:47:51 +0600

    payment page

--- app/src/main/java/com/example/bluetooth2/Payment.java
+++ app/src/main/java/com/example/bluetooth2/Payment.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class Payment extends AppCompatActivity {
 
     int pressedButtonNumber;
+    boolean payByQR = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,35 +18,42 @@ public class Payment extends AppCompatActivity {
         setContentView(R.layout.activity_payment);
 
         pressedButtonNumber = getIntent().getExtras().getInt("buttonNumber");
-        TextView tv1 = (TextView)findViewById(R.id.textView2);
-
-//        TextView tv1 = (TextView) this.findViewById(R.id.textView1);
-
+        TextView tv1 = (TextView)findViewById(R.id.selectedProductName);
         switch (pressedButtonNumber) {
             case 1:
-//                tv1.setText("new text new text");
-                tv1.setText("" + getString(R.string.btn_shampoo));
-                System.err.println("!!! Button 1" + getString(R.string.btn_shampoo));
+                tv1.setText(getString(R.string.btn_shampoo));
                 break;
             case 2:
-//                tv1.setText((CharSequence) getString(R.string.btn_antiseptic));
-                System.err.println("!!! Button 2");
+                tv1.setText(getString(R.string.btn_antiseptic));
                 break;
             case 3:
-//                tv1.setText("kkkkkkkkkk");
-//                tv1.setText(getString(R.string.btn_liquid_soap));
-                System.err.println("!!! Button 3");
+                tv1.setText(getString(R.string.btn_liquid_soap));
                 break;
             case 4:
-//                tv1.setText(getString(R.string.btn_dish_soap));
-                System.err.println("!!! Button 4");
+                tv1.setText(getString(R.string.btn_dish_soap));
                 break;
         }
     }
+
 
     public void goToMainFromPayment(View view) {
         System.err.println("!!!!!!!!!!!!GO TO MAIN");
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.payByQR:
+                if (checked)
+                    payByQR = true;
+                    break;
+            case R.id.payByApp:
+                if (checked)
+                    payByQR = false;
+                    break;
+        }
     }
 }
